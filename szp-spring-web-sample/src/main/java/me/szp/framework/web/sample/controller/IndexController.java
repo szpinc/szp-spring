bb86277c68fadf8e08044941fea47b828a032030
GhostDog <szp@szp.me>
2019-05-09 16:25:28 +0800

    Initial commit

---
+++ szp-spring-web-sample/src/main/java/me/szp/framework/web/sample/controller/IndexController.java
@@ -0,0 +1,21 @@
+package me.szp.framework.web.sample.controller;
+
+import me.szp.framework.spring.web.annotation.Controller;
+import me.szp.framework.spring.web.annotation.RequestMapping;
+import me.szp.framework.spring.web.annotation.RequestParam;
+
+/**
+ * @author Sun Zhi Peng
+ * @version 1.0.0
+ * @date 2019/5/9 16:13
+ */
+@Controller
+@RequestMapping("/index")
+public class IndexController {
+
+    @RequestMapping("/test")
+    public String index(@RequestParam("content") String content) {
+        return content;
+    }
+
+}
